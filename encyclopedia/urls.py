ae8c36d4cd9a412b241a43eb66daf31250e0784c
nurgisa <apbaz.nurgisa@gmail.com>
2023-10-29 20:19:44 +0500

    add random page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<title>", views.get_entry_by_title, name="get_entry_by_title")
+    path("wiki/<title>", views.get_entry_by_title, name="get_entry_by_title"),
+    path("random", views.get_random, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.shortcuts import render
 
 from . import util
@@ -11,11 +13,26 @@ def index(request):
 def get_entry_by_title(request, title):
 
     content = util.get_entry(title)
+
     if not content:
-        return render(request, "encyclopedia/404.html",status=404)
+        return page_404(request)
 
     return render(request, "encyclopedia/entry_by_title.html", {
-
+        "title": title,
         "content": content
     })
 
+
+def get_random(request):
+    entries = util.list_entries()
+    count_entries = len(entries)
+    if count_entries < 0:
+        return page_404(request)
+
+    i = randint(0, count_entries-1)
+
+    return get_entry_by_title(request, entries[i])
+
+
+def page_404(request):
+    return render(request, "encyclopedia/404.html", status=404)
